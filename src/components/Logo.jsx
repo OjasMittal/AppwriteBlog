eb552f72d25a391b2f426e94aaca4f9d4ae6884b
Ojas Mittal <ojasfarm31@gmail.com>
2025-01-08 15:47:31 +0530

    initial commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <img
+    src="https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572-768x591.png"
+    alt="Logo"
+    style= {{width:width}}
+    />
+  )
+}
+
+export default Logo
